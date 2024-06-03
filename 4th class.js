f9f266c334a94069d306ec5c6982ed054cd26423
wajeeha-arain <arainwajeeha715@gmail.com>
2024-06-03 10:02:48 +0500

    add-loop

---
+++ 4th class.js
@@ -0,0 +1,93 @@
+
+// console.log(5 != 7);
+
+// let ice = +prompt("inter your number");
+// if(ice === 100){
+//     console.log("ice-Cream is corenetto ");
+
+// }else if(ice === 500){
+//     console.log(".ice-Cream is corenetto family pack 1.5 LTR");
+// }else if(ice === 1000){
+//     console.log(".ice-Cream is corenetto family pack 2.5 LTR");
+// }else{
+//     console.log("your ice-Cearm is random ");
+// }
+
+
+// let ice = +prompt("what is your items")
+// switch (ice) {
+//     case 100:
+//            console.log("ice-Cream is corenetto ");
+//            break;
+//         case 500:
+//             console.log(".ice-Cream is corenetto family pack 1.5 LTR");
+//             break;
+//          case 1000:
+//             console.log(".ice-Cream is corenetto family pack 2.5 LTR");
+//             break;
+//     default:
+//         console.log("your ice-Cearm is random ");
+//         break;
+// }
+
+
+// let usernum = prompt("Even num");
+// let compNumber = Math.round(Math.random( ) * 10);
+
+// if(compNumber % 2 === 0 && usernum === "even" ){
+//     console.log(" you win ! "  + compNumber);
+// }else if( compNumber % 2 !== 0  && usernum === "odd"){
+//     console.log(" you win ! "  + compNumber);
+// }else{
+//     console.log(" you loss! "  + compNumber);
+// }
+//reminder %
+//......................'loop'............................///
+//loop
+//initialization
+//condition
+//increment repect
+// for(let i = 0 ; i <= 10 ; i ++){
+//     console.log(" hello world");
+// }
+
+// for(let i = 1 ; i <= 100 ; i ++){
+//          console.log(" hello world" + i);
+//      }
+
+// for(let i = 1 ; i <= 10 ; i ++){
+//     console.log("2 x " + i + " = " + 2 *i );
+// }
+
+// let table = 9;
+// for(let i = 1 ; i <= 10 ; i++){
+//     console.log( table + " x " + i + " = " + table *i );
+// }
+
+
+// let table = 9;
+//  for(let i = 1 ; i <= 10 ; i++){
+//     console.log( table + " x " + i + " = " + table *i );
+// console.log( Math.round(Math.random( ) * 10));
+// console.log("hello world")
+
+// }
+
+// for(let i = 1 ; i <= 10 ; i++){
+//     console.log("45 x " + i + " = " + 45 * i);
+// }
+
+// let usernum = prompt("odd num");
+
+// let computerNumber = Math.round(Math.random() * 10);
+
+// if( computerNumber % 2 === 0 && usernum === "even"){
+//     console.log(" you win ! " + computerNumber);
+//     // document.write(" you win ! " + computerNumber)
+// }else if( computerNumber % 2 !== 0 && usernum === "odd"){
+//     console.log("you win  odd num!  " + computerNumber );
+// }else{
+//     console.log(" you loss ! " + computerNumber);
+// }
+
+
