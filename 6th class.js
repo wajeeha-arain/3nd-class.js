fa02ec4889dfc3080d47240f68a64725e74899f3
wajeeha-arain <arainwajeeha715@gmail.com>
2024-06-04 20:17:45 +0500

    add-array

---
+++ 6th class.js
@@ -0,0 +1,54 @@
+
+//...........Array................. it is container .. we can store  multipli data in single varible.  
+//[] sequre backet
+
+
+// let arr = [ "ali " , " hadi" , "ahmed", "linta"]
+// //arr[0] = " ali4"// not re commended
+// // arr.push(" ali bux")/// array last value add end 
+// //arr.unshift("ali ahmed")//array first value addad
+// arr.pop()// array last value remove
+// arr.shift() //first value remove 
+// console.log(arr);
+
+// //arr.push...array last value add end 
+// let arr = [ "ali " , " hadi" , "ahmed", "linta"]
+// arr.push(bux)
+// console.log(arr);
+
+
+//arr.unshift..array first value addad
+// let arr = [ "ali " , " hadi" , "ahmed", "linta"]
+// arr.unshift(wajeeha)
+// console.log(arr);
+
+//arr.pop..array last value remove
+// let arr = [ "ali " , " hadi" , "ahmed", "linta"]
+// arr.pop()
+// console.log(arr);
+
+//arr.shift..first value remove 
+// let arr = [ "ali " , " hadi" , "ahmed", "linta"]
+// arr.shift()
+// console.log(arr);
+
+// splice add(replace) and remove
+//first ma add(replace) 2nd ma remove 
+// let arr = [ "ali " , " hadi" , "ahmed", "linta"]
+// arr.splice(1, 0 , "danish");
+// console.log(arr);
+
+// let arr = [ "ali " , " hadi" , "ahmed", "linta"]
+// arr.splice(1, 2, "danish");//remove ahmed
+// console.log(arr);
+
+// let a = [ 1,2,3,4,5,6,7,8]
+// console.log(a);
+// console.log(a.length);
+
+// slice is create new array .... 
+// let arr = [ "ali " , " hadi" , "ahmed", "linta"]
+// let b = arr.slice(0 ,3) // '0' is sterting point and '3' is ending point
+// console.log(arr);
+// console.log(b);
+
