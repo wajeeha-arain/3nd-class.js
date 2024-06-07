715f00558c12564e3d08ad22ea16347c0a136f8e
wajeeha-arain <arainwajeeha715@gmail.com>
2024-06-07 16:37:41 +0500

    add-objects

---
+++ 8th class.js
@@ -0,0 +1,89 @@
+//object..........
+// //pair of 3 values
+// let person2 = {
+//     name : "wasif",
+//     cnic : 23-45456-986,
+//     language : "sindhi",
+//     class :" javascipt",
+//     speck : function () {// those function comes in object that is called method..
+       
+//     }
+// } 
+// console.log(person2);
+
+//2
+// let person4 = {
+//     name : "wasif",
+//     cnic : 23-45456-986,
+//     language : "sindhi",
+//     class :" javascipt",
+//     speck : function (student) {// those function comes in object that is called method..
+//        console.log(student + this.name);
+//     }
+// }
+
+// let person3 = {
+//     name : "ali",//property
+//     cnic : 23-4986,//key
+//     language : "sindhi",
+//     class :" javascipt",
+//     speck : function (student) {// those function comes in object that is called method..
+//        console.log(student + this.name);
+//     }
+// }
+// person4.speck("hello i am ")
+// person3.speck("hello i am ")
+
+//3
+// let person = {
+//     name : "wasif",
+//     cnic : 23-45456-986,
+//     language : "sindhi",
+//     class :" javascipt",
+//     speck : function (student) {
+//        console.log(student + this.name);
+//     }
+// }
+
+// let person1 = {
+//     name : "ali",
+//     cnic : 23-4986,
+//     language : "sindhi",
+//     class :" javascipt",
+//     speck : function (student) {
+//        console.log(student + this.name);
+//     }
+//  }
+
+// let student = [
+//     person , person1
+// ];
+// console.log(student);
+
+
+//4
+// function student( name1 , cnic1, language1 ,javascipt1 ) {
+//     return{
+        
+//         name :name1,
+//         cnic :cnic1,
+//         language : language1,
+//         class : javascipt1,
+//     }
+// }
+// let student1 =  [student("ali", 3333, "sindhi", "html")]
+// let student2 = [student()]
+// console.log(student1);
+
+//5
+// function student()  {
+//         return{
+            
+//             name :prompt("inter your name"),
+//             cnic :+prompt("inter your cnic"),
+//             language : prompt("inter your language"),
+//             class : prompt("inter your class"),
+//         }
+//     }
+//     let stu= [student()]
+//     console.log(stu);
